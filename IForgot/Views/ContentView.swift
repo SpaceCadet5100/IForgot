c5654f745d0b360e82e309cbfaf6a949b7d1933a
Jedrek Haberka-Dawidek <jhaberka@C07GJ0GHQ6NY.local>
2023-02-15 14:03:54 +0100

    async fix

--- IForgot/Views/ContentView.swift
+++ IForgot/Views/ContentView.swift
@@ -10,31 +10,67 @@ import SwiftUI
 
 struct ContentView: View {
     @State var results = [NasaAPIModel]();
+    @State var nasaData: NasaAPIModel?
     var body: some View {
-        Text("AAAA")
-            .task {
-                print("Do your goddam job")
-                await LoadData()
-            }
+        Text(getString()).onAppear(perform: LoadData)
+        
+         
     }
-    func LoadData() async{
-            guard let url = URL (string: "https://api.nasa.gov/planetary/apod?api_key=itcCI2jHfrVY3pbsghGsaSzPIhsgpvsuM5pcdBao") else {
-            print("Invalid ur")
+    
+    func getString() -> String{
+        var string = "Not found lol"
+        if let nasaData = nasaData {
+            string = nasaData.url
+        }
+        return string
+    }
+    
+    func LoadData() {
+        guard let url = URL (string: "https://api.nasa.gov/planetary/apod?api_key=itcCI2jHfrVY3pbsghGsaSzPIhsgpvsuM5pcdBao") else {
+            print("Invalid url")
             return
         }
-        do{
-            let (data, _) = try await URLSession.shared.data(from: url)
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from:  data){
-                    results = decodedResponse.NasaAPIModels
-                    print(results)
-                    print("BLALALA")
-                }
-                
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request)
+        {
+            data, response, error in
+            
+            if let error = error {
+                print("Error: fetch failed: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("ERROR: failed to get data form URLSession")
+                return
+            }
+            
+            var newNasaData: NasaAPIModel?
+            do {
+                newNasaData = try JSONDecoder().decode(NasaAPIModel.self, from:data)
+            }
+            //todo moet nog gefixed worden 
+            
+            catch{
+                print("woopsie")
+            }
+            
+            if newNasaData == nil {
+                print("woopsie 2")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.nasaData = newNasaData
+
+            }
+            
         }
-        catch{
-            print("datafromURL failed")
+        
+        task.resume()
+    
         }
-    }
 }
 
 
