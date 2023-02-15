9a04421fa78e018e1f3f770a4fdb51f7396dced6
Marijn <marijnvandenbos59@gmail.com>
2023-02-15 09:44:41 +0100

    loading screen

--- IForgot/Views/SplashView.swift
+++ IForgot/Views/SplashView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isRotating = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Text("loading our application")
+            Image("loading")
+                .imageScale(.small)
                 .foregroundColor(.accentColor)
-            Text("Welcome to IForgot we forgot")
+                .rotationEffect(.degrees(isRotating))
+                            .onAppear {
+                                withAnimation(.linear(duration: 1)
+                                        .speed(0.1).repeatForever(autoreverses: false)) {
+                                    isRotating = 360.0
+                                }
+                            }
+                .padding()
+            Text("loading our application")
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        SplashView()
-    }
-}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            SplashView()
+        }
+    }}
