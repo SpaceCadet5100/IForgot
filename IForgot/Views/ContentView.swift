d6407f36acb257f15eaf67eed564a5a0d3b548d6
Marijn <marijnvandenbos59@gmail.com>
2023-02-16 12:15:10 +0100

    zonder catches

--- IForgot/Views/ContentView.swift
+++ IForgot/Views/ContentView.swift
@@ -1,66 +1,51 @@
-//
-//  ContentView.swift
-//  IForgot
-//
-//  Created by Marijn van den Bos on 15/02/2023.
-//
-
 import SwiftUI
 
 
 struct ContentView: View {
     @State var results = [NasaAPIModel]();
+    @State var nasaData: NasaAPIModel?
+    
     var body: some View {
-        List{
-            ForEach(results, id: \.date){ results in
-                VStack{
-                    Text(results.url)
-                }
-            }
+        VStack{
+        Text("a")
         }.onAppear(perform: LoadData)
-        
-        func LoadData() {
-            guard let url = URL (string: "https://api.nasa.gov/planetary/apod?api_key=itcCI2jHfrVY3pbsghGsaSzPIhsgpvsuM5pcdBao&count=5") else {
-                print("Invalid url")
-                return
-            }
-            
-            let request = URLRequest(url: url)
-            let task = URLSession.shared.dataTask(with: request)
-            {
-                data, response, error in
-                
-                if let error = error {
-                    print("Error: fetch failed: \(error.localizedDescription)")
-                    return
-                }
-                
-                guard let data = data else {
-                    print("ERROR: failed to get data form URLSession")
-                    return
-                }
-                
-                do {
-                    results = try JSONDecoder().decode([NasaAPIModel].self, from:data)
-                }
-                //todo moet nog gefixed worden
-                
-                catch{
-                    print("woopsie")
-                }
-                
-                
-                DispatchQueue.main.async {
-                    self.results = results
-                    
-                }
-                
-            }
-            
-            task.resume()
+    }
+     
+    
+    func LoadData() {
+        guard let url = URL (string: "https://api.nasa.gov/planetary/apod?api_key=itcCI2jHfrVY3pbsghGsaSzPIhsgpvsuM5pcdBao&count=5") else {
+            print("Invalid url")
             return
         }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request)
+        {
+            data, response, error in
+            var newResults = results
+            if let error = error {
+                print("Error: fetch failed: \(error.localizedDescription)")
+                return
+            }
+         
+            guard let data = data else {
+                print("ERROR: failed to get data form URLSession")
+                return
+            }
+            do {
+                newResults = try JSONDecoder().decode([NasaAPIModel].self, from:data)
+                print(newResults[0].url)
+            }
+            catch{
+                print("woopsie")
+            }
+            DispatchQueue.main.async {
+            self.results = newResults
+
+            }
+        }
+        task.resume()
     }
+    
     
     
     struct ContentView_Previews: PreviewProvider {
