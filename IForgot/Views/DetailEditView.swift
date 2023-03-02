26f3d2b27dcf15a3eb7b0ff03d7e76f9d534009b
Jedrek Haberka-Dawidek <jhaberka@C07GJ0GHQ6NY.local>
2023-03-02 20:50:53 +0100

    changes

---
+++ IForgot/Views/DetailEditView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailEditView.swift
+//  IForgot
+//
+//  Created by Jedrek Haberka-Dawidek on 02/03/2023.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
